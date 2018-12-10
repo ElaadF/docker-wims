fc40b78a27e2fd3296a2ef15ae8f37d49ba44e07
FURREEDAN ELAAD <efurreed@localhost.localdomain>
2018-12-10 18:04:48 +0100

    Schema of execution

--- runWims.sh
+++ runWims.sh
@@ -1,3 +1,6 @@
+# si il existe un container qui porte le nom <name>
+#		docker container start <container's name>
+#sinon
 docker build -t wimsimage .
 docker run -itd -p 5050:80 -v "$(pwd)"/DATA:/DATA --name wimscontainer wimsimage
 docker exec -it wimscontainer ./bin/apache-config
